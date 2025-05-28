b6a52089fdfd89d49e00ae8bfef7f8e7957eddc7
Jared Hutter <jh@aptitudere.com>
2025-05-29 00:22:28 +0800

    Fix: remove authenticate and use token.json directly

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
 from flask import Flask, request, jsonify
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 import os
 
 app = Flask(__name__)
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
-creds = None
 
-def authenticate():
-    global creds
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-        creds = flow.run_local_server(port=8080)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-# authenticate()
+# Use pre-generated token.json directly
+creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
 @app.route('/read-file', methods=['GET'])
 def read_file():
